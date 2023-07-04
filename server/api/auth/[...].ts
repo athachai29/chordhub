6b403a79bdc3e44fb9567751d58b3546bc120528
ppppp313 <athachai.m@gmail.com>
2023-07-04 20:55:30 +0700

    use env instead

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -7,28 +7,10 @@ export default NuxtAuthHandler({
     signIn: "/login",
   },
   // your authentication configuration here!
-  secret: "your-secret-here",
+  secret: process.env.JWT_SECRET,
   providers: [
     // @ts-ignore Import is exported on .default during SSR, so we need to call it this way. May be fixed via Vite at some point
     CredentialsProvider.default({
-      // The name to display on the sign in form (e.g. 'Sign in with...')
-      // name: "Credentials",
-      // // The credentials is used to generate a suitable form on the sign in page.
-      // // You can specify whatever fields you are expecting to be submitted.
-      // // e.g. domain, username, password, 2FA token, etc.
-      // // You can pass any HTML attribute to the <input> tag through the object.
-      // credentials: {
-      //   username: {
-      //     label: "Username",
-      //     type: "text",
-      //     placeholder: "(hint: jsmith)",
-      //   },
-      //   password: {
-      //     label: "Password",
-      //     type: "password",
-      //     placeholder: "(hint: hunter2)",
-      //   },
-      // },
       authorize(credentials: any) {
         // You need to provide your own logic here that takes the credentials
         // submitted and returns either a object representing a user or value
