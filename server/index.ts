0e4081a843d8933a9b805a9367014733d091ade1
ppppp313 <athachai.m@gmail.com>
2023-07-11 23:08:06 +0700

    doc: update db log

--- server/index.ts
+++ server/index.ts
@@ -5,8 +5,8 @@ const config = useRuntimeConfig();
 export default async () => {
   try {
     await mongoose.connect(config.mongoUrl);
-    console.log("Database connection established.");
+    console.log("Database connection established");
   } catch (err) {
-    console.error("Database connection failed.", err);
+    console.error("Database connection failed");
   }
 };
