f9546473d6cd9587af828d47f6ae43aae303da96
ppppp313 <athachai.m@gmail.com>
2023-07-17 10:33:30 +0700

    feat: add return custom data

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -1,34 +1,46 @@
 import CredentialsProvider from "next-auth/providers/credentials";
 import { NuxtAuthHandler } from "#auth";
 
+import userModel from "../../models/users";
+
 export default NuxtAuthHandler({
   pages: {
     // Change the default behavior to use `/login` as the path for the sign-in page
     signIn: "/login",
+  },
+  callbacks: {
+    // Callback when the JWT is created / updated, see https://next-auth.js.org/configuration/callbacks#jwt-callback
+    jwt: async ({ token, user }) => {
+      const isSignIn = user ? true : false;
+      if (isSignIn) {
+        token.jwt = user ? (user as any).access_token || "" : "";
+        token.id = user ? user.id || "" : "";
+        token.role = user ? (user as any).role || "" : "";
+      }
+      return Promise.resolve(token);
+    },
+    // Callback whenever session is checked, see https://next-auth.js.org/configuration/callbacks#session-callback
+    session: async ({ session, token }) => {
+      (session as any).role = token.role;
+      (session as any).uid = token.id;
+      return Promise.resolve(session);
+    },
   },
   // your authentication configuration here!
   secret: process.env.AUTH_SECRET,
   providers: [
     // @ts-ignore Import is exported on .default during SSR, so we need to call it this way. May be fixed via Vite at some point
     CredentialsProvider.default({
-      authorize(credentials: any) {
-        // You need to provide your own logic here that takes the credentials
-        // submitted and returns either a object representing a user or value
-        // that is false/null if the credentials are invalid.
-        // NOTE: THE BELOW LOGIC IS NOT SAFE OR PROPER FOR AUTHENTICATION!
-        const user = {
-          id: "1",
-          name: "Athachai Mala-ut",
-          username: "mala.ut.29@gmail.com",
-          password: "password",
-          image: "https://avatars.githubusercontent.com/u/25911230?v=4",
-        };
+      async authorize(credentials: any) {
+        const { username, password } = credentials;
 
-        if (
-          credentials?.username === user.username &&
-          credentials?.password === user.password
-        ) {
+        const user = await userModel.findOne({ email: username });
+        const validate = await user?.verifyPassword(password);
+
+        if (validate) {
           // Any object returned will be saved in `user` property of the JWT
+          user.id = user._id;
+
           return user;
         } else {
           // eslint-disable-next-line no-console
