7a80497cb9c378a8ecf6222417ef7196b669b30e
ppppp313 <athachai.m@gmail.com>
2023-07-17 10:31:08 +0700

    feat: update profile page

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,3 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const { data } = useAuth();
 
-<template>profile</template>
+const totalRequest = ref(0);
+
+onMounted(async () => {
+  const response = await fetch(`/api/songs/requests/${data.value.uid}`);
+  const result = await response.json();
+
+  totalRequest.value = result.total;
+});
+</script>
+
+<template>
+  <div class="px-16 mt-12 flex flex-col gap-4">
+    <div class="text-2xl">Profile</div>
+    <img class="w-24 mt-2" :src="data?.user?.image" />
+    <div>{{ data?.user?.name }}</div>
+    <div>Total request: {{ totalRequest }}</div>
+  </div>
+</template>
