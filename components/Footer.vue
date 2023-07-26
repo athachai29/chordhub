ce78a6b35f85993505949405fdc84acb85217900
ppppp313 <athachai.m@gmail.com>
2023-07-26 11:23:52 +0700

    add buymeacoffee button

--- components/Footer.vue
+++ components/Footer.vue
@@ -4,7 +4,16 @@ import pkgJSON from "../package.json"
 
 <template>
   <footer class="text-black p-6 flex justify-between">
-    <span>Made with Love and Depression</span>
+    <span class="flex items-center">
+      <span class="mr-2">Made with Love and Depression</span>
+      <span>
+        <a href="https://www.buymeacoffee.com/athachai.m"
+          ><img
+            src="https://www.buymeacoffee.com/assets/img/custom_images/yellow_img.png"
+            width="128px"
+        /></a>
+      </span>
+    </span>
     <span>v{{ pkgJSON.version }}</span>
   </footer>
 </template>
