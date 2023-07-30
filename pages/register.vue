f9eda664a9c867117d442cd46feebdba84939780
ppppp313 <athachai.m@gmail.com>
2023-07-31 02:06:22 +0700

    feat: add register function

--- pages/register.vue
+++ pages/register.vue
@@ -1,8 +1,4 @@
 <script setup lang="ts">
-const { signIn } = useAuth()
-const router = useRouter()
-const route = useRoute()
-
 definePageMeta({
   auth: {
     unauthenticatedOnly: true,
@@ -11,27 +7,33 @@ definePageMeta({
 })
 
 const form = ref({
-  username: "mala.ut.29@gmail.com",
-  password: "password",
-  callbackUrl: (router.options.history.state.back as string) || "/",
+  username: "",
+  password: "",
+  callbackUrl: (useRouter().options.history.state.back as string) || "/",
 })
 
-const onLogin = async () => {
+const onRegisterWithCredentials = async () => {
   /**
    * Google Analytics
    */
-  gtag("event", "login", { method: "Credentials" })
+  gtag("event", "register", { method: "Credentials" })
 
-  await signIn("credentials", form.value)
+  await fetch("/api/auth/register", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(form.value),
+  })
+
+  await useAuth().signIn("credentials", form.value)
+
+  useRouter().push("/")
 }
 
-const onLoginWithGoogle = async () => {
+const onRegisterWithGoogle = async () => {
   /**
    * Google Analytics
    */
-  gtag("event", "login", { method: "Google" })
-
-  await signIn("google")
+  gtag("event", "register", { method: "Google" })
 }
 
 /**
@@ -44,9 +46,9 @@ gtag("set", "page_title", "Login")
 <template>
   <div class="flex h-screen flex-col">
     <main class="flex flex-grow flex-col justify-center px-6 py-12 lg:px-8">
-      <h1 class="mt-10 text-center text-2xl">Log in to your account</h1>
+      <h1 class="mt-10 text-center text-2xl">Create new account</h1>
       <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form class="space-y-6" @submit.prevent="onLogin">
+        <form class="space-y-6" @submit.prevent="onRegisterWithCredentials">
           <div>
             <label for="email" class="block">Email</label>
             <div class="mt-2">
@@ -58,6 +60,7 @@ gtag("set", "page_title", "Login")
                 required
                 class="block w-full border border-black px-4 py-2 focus:outline-none"
                 v-model="form.username"
+                placeholder="Enter email"
               />
             </div>
           </div>
@@ -68,9 +71,6 @@ gtag("set", "page_title", "Login")
                 class="block text-sm font-medium leading-6 text-gray-900"
                 >Password</label
               >
-              <div class="text-sm">
-                <a href="#" class="font-semibold">Forgot password?</a>
-              </div>
             </div>
             <div class="mt-2">
               <input
@@ -81,6 +81,28 @@ gtag("set", "page_title", "Login")
                 required
                 class="block w-full border border-black px-4 py-2 focus:outline-none"
                 v-model="form.password"
+                placeholder="Enter password"
+              />
+            </div>
+          </div>
+          <div>
+            <div class="flex items-center justify-between">
+              <label
+                for="password"
+                class="block text-sm font-medium leading-6 text-gray-900"
+                >Confirm Password</label
+              >
+            </div>
+            <div class="mt-2">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autocomplete="current-password"
+                required
+                class="block w-full border border-black px-4 py-2 focus:outline-none"
+                v-model="form.password"
+                placeholder="Confirm your entered password"
               />
             </div>
           </div>
@@ -89,15 +111,18 @@ gtag("set", "page_title", "Login")
               type="submit"
               class="w-full border-2 border-black px-4 py-2 hover:bg-black hover:text-white"
             >
-              Log in
+              Create account
             </button>
-            <div v-if="route.query.error" class="mt-2 text-right text-red-600">
+            <div
+              v-if="useRoute().query.error"
+              class="mt-2 text-right text-red-600"
+            >
               Username or password is incorrect
             </div>
           </div>
           <div class="relative flex items-center py-5">
             <div class="flex-grow border-t"></div>
-            <span class="mx-4 flex-shrink">or Log in with</span>
+            <span class="mx-4 flex-shrink">or</span>
             <div class="flex-grow border-t"></div>
           </div>
           <div>
@@ -105,18 +130,18 @@ gtag("set", "page_title", "Login")
               type="submit"
               class="mt-2 w-full border-2 border-black px-4 py-2 hover:bg-[#3b5998] hover:text-white"
             >
-              Facebook
+              Continue with Facebook
             </button> -->
             <button
               class="mt-2 w-full border-2 border-black px-4 py-2 hover:bg-[#dd4b39] hover:text-white"
-              @click="() => onLoginWithGoogle()"
+              @click="() => onRegisterWithGoogle()"
             >
-              Google
+              Continue with Google
             </button>
           </div>
         </form>
         <div class="mt-10 text-center text-sm text-gray-500">
-          <NuxtLink to="/register"> Not have an account? </NuxtLink>
+          <NuxtLink to="/login">Already have an account? Login</NuxtLink>
         </div>
       </div>
     </main>
