1ad533e8d7ee4743d21dc4f1f2c4415b62801dcb
ppppp313 <athachai.m@gmail.com>
2023-07-31 11:30:13 +0700

    feat: add login with google

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -1,24 +1,41 @@
 import CredentialsProvider from "next-auth/providers/credentials"
+import GoogleProvider from "next-auth/providers/google"
 import { NuxtAuthHandler } from "#auth"
-
-import userModel from "../../models/users"
+import Users from "../../models/users"
 
 export default NuxtAuthHandler({
   pages: {
     signIn: "/login",
+    signOut: "/logout",
+    error: "/auth/error", // Error code passed in query string as ?error=
+    verifyRequest: "/auth/verify-request", // (used for check email message)
+    newUser: "/auth/new-user", // New users will be directed here on first sign in (leave the property out if not of interest)
   },
   callbacks: {
     async signIn({ user, account, profile, email, credentials }) {
-      return true
+      try {
+        if (account.provider === "credentials") {
+          console.log("signIn", { user, account, profile, email, credentials })
+        }
+        if (account.provider === "google") {
+          console.log({ account, profile })
+        }
+        return true
+      } catch (err) {
+        console.log(err)
+        return false
+      }
     },
     async redirect({ url, baseUrl }) {
       return baseUrl
     },
-    async session({ session, user, token }) {
-      return session
-    },
     async jwt({ token, user, account, profile, isNewUser }) {
+      console.log("jwt", { token, user, account, profile, isNewUser })
       return token
+    },
+    async session({ session, user, token }) {
+      console.log("session", { session, user, token })
+      return session
     },
   },
   secret: process.env.AUTH_SECRET,
@@ -27,7 +44,7 @@ export default NuxtAuthHandler({
       async authorize(credentials: any, req: any) {
         const { username, password } = credentials
 
-        const user = await userModel.findOne({ email: username })
+        const user = await Users.findOne({ email: username })
         if (!user) return null
 
         const validate = await user?.verifyPassword(password)
@@ -37,5 +54,12 @@ export default NuxtAuthHandler({
         return user
       },
     }),
+    GoogleProvider.default({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
   ],
+  session: {
+    strategy: "jwt",
+  },
 })
