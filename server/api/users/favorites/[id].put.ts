8f1f81c0ea8bc1875ed6e4ed8c41ed2ab432e695
ppppp313 <athachai.m@gmail.com>
2023-09-12 22:04:46 +0700

    feat: add get, add and remove favorite songs feature

---
+++ server/api/users/favorites/[id].put.ts
@@ -0,0 +1,23 @@
+import userModel from "../../../models/users"
+
+export default defineEventHandler(async (event) => {
+  const { sub: userId } = event.context.auth
+
+  const { id } = getRouterParams(event)
+
+  const updatedResponse = await userModel.findByIdAndUpdate(
+    userId,
+    {
+      $addToSet: {
+        favorites: id,
+      },
+    },
+    { new: true },
+  )
+
+  return {
+    status: 200,
+    success: true,
+    data: updatedResponse,
+  }
+})
---
+++ server/api/users/favorites/index.get.ts
@@ -0,0 +1,16 @@
+import userModel from "../../../models/users"
+
+export default defineEventHandler(async (event) => {
+  const { sub: userId } = event.context.auth
+
+  const user = await userModel.findById(userId, "favorites").populate({
+    path: "favorites",
+    select: "title thaiName engName",
+  })
+
+  return {
+    status: 200,
+    success: true,
+    data: user?.favorites || [],
+  }
+})
