eb4dcacd4f558d2451f92e316bebe7300a9aa431
ppppp313 <mala.ut.29@gmail.com>
2023-12-31 01:20:51 +0700

    feat: apply ii-format for sheet

--- components/Sheet.vue
+++ components/Sheet.vue
@@ -12,13 +12,14 @@ const formatter = (sheet: string[], format: any) => {
 }
 
 // TODO: Select format from device screen
-const sheet = ref(formatter(props.rawSheet, iFormat))
+// const sheet = ref(formatter(props.rawSheet, iFormat))
+const sheet = ref(formatter(props.rawSheet, iiFormat))
 </script>
 
 <template>
   <ul class="my-6 font-mono">
     <li v-for="(line, index) in sheet" :key="index">
-      <div class="mb-4" v-html="line"></div>
+      <div class="mb-5" v-html="line"></div>
     </li>
   </ul>
 </template>
@@ -31,6 +32,7 @@ const sheet = ref(formatter(props.rawSheet, iFormat))
 .chord .inner {
   position: relative;
   top: -1.25em;
+  left: -1.25em;
   font-family: "Roboto Mono", monospace;
 }
 </style>
