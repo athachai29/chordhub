2434c9c0b01e436a91a84a3d04df1d7b87047d47
ppppp313 <mala.ut.29@gmail.com>
2024-01-01 23:15:57 +0700

    fix: use email as a fallback in case name is undefined

--- stores/avatar.ts
+++ stores/avatar.ts
@@ -1,8 +1,12 @@
 import { defineStore } from "pinia"
 
 export const useAvatar = defineStore("avatar", {
-  state: () => ({
-    url: `${useRuntimeConfig().public.dicebearApiUrl}?seed=${useAuth().data
-      .value?.user?.name}`,
-  }),
+  state: () => {
+    const { data } = useAuth()
+    return {
+      url: `${useRuntimeConfig().public.dicebearApiUrl}?seed=${
+        data.value?.user?.name || data.value?.user?.email
+      }`,
+    }
+  },
 })
