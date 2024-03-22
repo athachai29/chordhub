b2517653b2f33d95dc5370318c6b91992f20037d
ppppp313 <mala.ut.29@gmail.com>
2024-03-23 02:54:44 +0700

    Add Button, Input, and SearchBar components

---
+++ components/base/Button.vue
@@ -0,0 +1,17 @@
+<script setup>
+const props = defineProps(["placeholder", "type", "btnStyle"])
+
+const classes = reactive({
+  "bg-black text-white": props.btnStyle === "primary",
+})
+</script>
+
+<template>
+  <button
+    :type="props.type || 'button'"
+    class="rounded-lg border-2 border-black px-4 py-2 hover:bg-black hover:text-white"
+    :class="classes"
+  >
+    <slot></slot>
+  </button>
+</template>
---
+++ components/base/Input.vue
@@ -0,0 +1,13 @@
+<script setup>
+const props = defineProps(["placeholder"])
+const model = defineModel()
+</script>
+
+<template>
+  <input
+    type="text"
+    class="block w-full rounded-lg border border-2 border-black px-4 py-2 focus:outline-none"
+    :placeholder="props.placeholder"
+    v-model="model"
+  />
+</template>
